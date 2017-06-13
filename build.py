15a2b15d8be5551f05e2d70ba94b59e479c3db24
coded9 <coded9@users.noreply.github.com>
2017-06-13 04:40:16 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,10 @@
-def solution(pd):
+import pandas as pd
+def solution(series):
     """
     Enter your code here
     """
+    return list(series)
+
+series = pd.Series([1,2,3,4,5])
+print series
+print solution(series)    
