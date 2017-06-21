a5836d1ed3444f418cce3a66226747e64b2f89aa
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-21 14:22:11 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,15 @@
-def solution(pd):
-    """
-    Enter your code here
-    """
+
+import pandas as pd
+
+
+
+
+input = pd.Series([2, 4, 6, 8, 10])
+
+def solution(array):
+    arr = pd.Series(array).tolist()
+    return (arr)
+
+
+
+solution(input)
