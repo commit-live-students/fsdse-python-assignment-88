d9644e8af848d25512bf996a15fab1a4bbd47c3a
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-09-24 08:17:52 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
-def solution(pd):
-    """
-    Enter your code here
-    """
+import pandas as pd
+def solution(series):
+    return series.tolist()
+
+s1 = pd.Series([i for i in range(2,11,2)])
+s1.tolist()
